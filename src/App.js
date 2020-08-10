8fed90cfd9816f50d9d3e790e690c30807b6bd4a
keshavdulal <keshav.dulal@gmail.com>
2020-08-10 15:51:15 +0545

    dev : add reach router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt me</h1>
-      {/* <Pet name="luna" animal="dog" breed="havanese" />
-      <Pet name="tuna" animal="fish" breed="aquatic" />
-      <Pet name="muna" animal="cat" breed="mixed" /> */}
-      <SearchParams />
+      <header>
+        <Link to="/">Adopt me</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
