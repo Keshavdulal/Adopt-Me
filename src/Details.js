30f5e177613b7f011abc6b957c6dac9e2468a1ea
keshavdulal <keshav.dulal@gmail.com>
2020-08-11 13:45:27 +0545

    add details page for pets

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,63 @@
-import React from "react";
-const Details = () => {
-  return <div>Details</div>;
-};
+import React, { Component } from "react";
+import pet from "@frontendmasters/pet";
+import Caraousel from "./Carousel";
+import ErrorBoundaries from "./ErrorBoundaries";
 
-export default Details;
+class Details extends Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    // throw new Error("something went wrong");
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    const {
+      loading,
+      name,
+      animal,
+      breed,
+      location,
+      description,
+      media,
+    } = this.state;
+
+    return (
+      <div>
+        {loading ? (
+          <h1>Loading Data</h1>
+        ) : (
+          <div className="details">
+            <Caraousel media={media} />
+            <div>
+              <h1>{name}</h1>
+              <h2>{`${animal} - ${breed} - ${location}`}</h2>
+              <p>{description}</p>
+              <button>Adopt {name}</button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+//HOC Element
+export default function detailsWithErrorBoundaries(props) {
+  return (
+    <ErrorBoundaries>
+      {/* be cautious with spread operator - decreases readability */}
+      <Details {...props} />
+    </ErrorBoundaries>
+  );
+}
