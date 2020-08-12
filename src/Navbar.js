256bde99db6318351e8dde81a1b5adcc19fa32a1
keshavdulal <keshav.dulal@gmail.com>
2020-08-12 18:48:11 +0545

    dev : add styled-comp Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,12 +6,20 @@ const Navbar = () => {
   return (
     <header
       css={css`
-        background-color: peru;
+        background-color: #d9c148;
         padding: 15px;
+        margin-bottom: 10px;
+        border-radius: 10px;
       `}
     >
       <Link to="/"> Adopt Me</Link>
-      <span role="img" aria-label="logo">
+      <span
+        css={css`
+          font-size: 60px;
+        `}
+        role="img"
+        aria-label="logo"
+      >
         🐶
       </span>
     </header>
