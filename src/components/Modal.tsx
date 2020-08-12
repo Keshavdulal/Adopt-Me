3555868f5fb30a2a688e10b0fb4d6349c2de624d
keshavdulal <keshav.dulal@gmail.com>
2020-08-12 20:40:23 +0545

    dev : add typescript and modify Modal.js to tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, FunctionComponent } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+// grab the modal we created in index.html
+const modalRoot = document.getElementById("modal");
+
+const Modal: FunctionComponent = ({ children }) => {
+  const elRef = useRef(document.createElement("div"));
 
   // always ref to the same element; don't create new ones
-  if (!elRef.current) {
-    const div = document.createElement("div");
-    elRef.current = div;
-  }
+  // if (!elRef.current) {
+  //   const div = document.createElement("div");
+  //   elRef.current = div;
+  // }
 
   useEffect(() => {
-    // grab the modal we created in index.html
-    const modalRoot = document.getElementById("modal");
+    if (!modalRoot) {
+      return;
+    }
 
     //append the newly created div to our modal
     modalRoot.appendChild(elRef.current);
 
     // at the end remove the "newly" created div using a cleanup func when the modal gets closed
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      modalRoot.removeChild(elRef.current);
+    };
   }, []); // [] -> means run this useEffect only once
 
   // coolest part here i.e. rendering our div to another part of DOM
